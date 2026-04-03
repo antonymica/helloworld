4ca7ca244d6808a8cb000705f7a649b1deb3bcb0
Micael Tahintsoa <mtahintsoa@gmail.com>
2026-04-03 11:34:26 +0300

    fix allowhosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ['helloworld.antonymica.site'],
+    allowedHosts: ['helloworld.antonymica.site', 'very-simple.duckdns.org'],
   },
 });
